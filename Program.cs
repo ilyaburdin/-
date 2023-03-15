9e644c5197a3b5a4f464775e04108a32c52c8395
ilya <ilya.burdin.87@bk.ru>
2023-03-15 09:10:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] spled = text.Split(new char[]{' ', ':','.',',', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+            var res = from word in spled
+                      where word.Length == 3
+                      select word;
+            if (res.Count() == 0)
+            {
+                Console.WriteLine("no 3 length words");
+            }
+            else
+            {
+                foreach (string s in res)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    
